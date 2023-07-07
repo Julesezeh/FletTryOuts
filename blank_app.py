b394128653b7a48f4024e566a59822fcd62e910a
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-07-07 14:34:09 +0100

    re-typed

--- blank_app.py
+++ blank_app.py
@@ -1,9 +1,7 @@
 import flet as ft
 
 def main(page: ft.Page):
-    t = ft.Text(value="Hello, world!", color="green")
+    t = ft.Text(value="Hello World", color="light green")
     page.controls.append(t)
     page.update()
-
-
 ft.app(target=main, view=ft.WEB_BROWSER)
