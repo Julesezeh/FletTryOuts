d0c60d533976ac115de86e9621d8da3a300ec0e5
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-07-10 11:36:05 +0100

    testing "greetings" control reference

---
+++ control_references.py
@@ -0,0 +1,21 @@
+import flet as ft
+
+def main(page: ft.Page):
+    first_name = ft.TextField(label="First Name")
+    last_name = ft.TextField(label="Last Name")
+    greetings = ft.Column()
+
+    def btn_click(e):
+        greetings.controls.append(ft.Text(value=f"Hello {first_name.value} {last_name.value}!"))
+        first_name.value = ""
+        last_name.value = ""
+        page.update()
+        first_name.focus()
+
+    page.add(
+        first_name,
+        last_name,
+        ft.ElevatedButton("Say Hello!", on_click=btn_click)
+    )
+
+ft.app(target=main)
