59052c5951a0ee5121fc55029c1c319a6c78d644
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-07-10 11:27:33 +0100

    tests the "disabled" property of controls(control)

--- using_disabled_property.py
+++ using_disabled_property.py
@@ -3,12 +3,8 @@ import flet as ft
 def main(page: ft.Page):
     first_name = ft.TextField(label="First name")
     last_name = ft.TextField(label="Last name")
-    c = ft.Column(controls=[
-        first_name,
-        last_name
-    ])
-    c.disabled = True   
-
-    page.add(c)
+    first_name.disabled  = True
+    last_name.disabled = True
+    page.add(first_name,last_name)
 
 ft.app(target=main)
