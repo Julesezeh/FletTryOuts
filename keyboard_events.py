bbcf106bd12beb4f6ad00e1c9be05de03a6f8a36
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-07-19 03:39:52 +0100

    the Meta key

--- keyboard_events.py
+++ keyboard_events.py
@@ -4,6 +4,7 @@ def main(page: ft.Page):
     def on_key(e: ft.KeyboardEvent):
         page.add(
             ft.Text(
+            #meta is for the macbook's command key
                 f"Key {e.key} :::[ Shift clicked-->{e.shift}, CNTRL clicked -->{e.ctrl},  Alt clicked-->{e.alt},   Meta clicked-->{e.meta}]"
             )
         )
