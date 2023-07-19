d41a1f4f9bbcde7aed72e59569f6550af0ebf4e5
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-07-19 03:35:23 +0100

    Keyboard event handler

---
+++ keyboard_events.py
@@ -0,0 +1,16 @@
+import flet as ft
+
+def main(page: ft.Page):
+    def on_key(e: ft.KeyboardEvent):
+        page.add(
+            ft.Text(
+                f"Key {e.key} :::[ Shift clicked-->{e.shift}, CNTRL clicked -->{e.ctrl},  Alt clicked-->{e.alt},   Meta clicked-->{e.meta}]"
+            )
+        )
+    page.on_keyboard_event = on_key
+    page.add(
+        ft.Text("Press any key with a combination of CNTRL, Shift, ALT and Meta ")
+    )
+
+
+ft.app(target=main)
