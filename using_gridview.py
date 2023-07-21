2ef5e7382d194fdc7aab94f4c5d6ab15d9f14322
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-07-21 04:27:33 +0100

    Alternative to gridView vontrol

---
+++ using_gridview.py
@@ -0,0 +1,27 @@
+import os
+import flet as ft
+
+#Grids are made by setting wrap=True in Column or Row controls 
+#(This is a more inefficient approach than using GridView control)
+
+os.environ["FLET_WS_MAX_MESSAGE_SIZE"] = "8000000"
+
+def main(page: ft.Page):
+    r = ft.Row(wrap=True, scroll="always", expand=True)
+    page.add(r)
+
+    for i in range(5000):
+        r.controls.append(
+            ft.Container(
+                ft.Text(f"Item {i}"),
+                width=100,
+                height=100,
+                alignment=ft.alignment.center,
+                bgcolor=ft.colors.GREEN_300,
+                border=ft.border.all(1, ft.colors.AMBER_400),
+                border_radius=ft.border_radius.all(5),
+            )
+        )
+    page.update()
+
+ft.app(target=main)
